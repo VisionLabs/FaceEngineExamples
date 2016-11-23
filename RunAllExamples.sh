0b5a796c225599c4c4828862b4a94c953fca9893
Sergey Adeykin <s.adeykin@visionlabs.ru>
2016-11-23 14:53:47 +0300

    Example2 with FSDK_v.2.4.3 interface

---
+++ RunAllExamples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ""
+
+echo ""
+echo "======================================="
+echo "== RUNNING EXAMPLE 1 -- VERIFICATION =="
+echo "======================================="
+echo ""
+
+./build/example1/Example1_Verification portrait.ppm portrait.ppm 0.7
+
+echo ""
+echo "======================================="
+echo "== RUNNING EXAMPLE 2 --  ATTRIBUTES  =="
+echo "======================================="
+echo ""
+
+./build/example2/Example2_Attributes portrait.ppm
+
+
+echo ""
+echo ""
