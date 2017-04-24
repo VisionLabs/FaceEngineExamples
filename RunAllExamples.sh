848bdb97221f34e0297cf0fa2e2537e3df3f8598
sava <a.savinskiy@visionlabs.ru>
2017-04-24 17:58:48 +0300

    fix bug

--- RunAllExamples.sh
+++ RunAllExamples.sh
@@ -8,7 +8,7 @@ echo "== RUNNING EXAMPLE 1 -- VERIFICATION =="
 echo "======================================="
 echo ""
 
-./build/example1/Example1 images/Cameron_Diaz.ppm images/Cameron_Diaz_2.ppm 0.7
+./build/example1/Example1 FaceEngineExamples/images/Cameron_Diaz.ppm images/Cameron_Diaz_2.ppm 0.7
 
 echo ""
 echo "======================================="
@@ -16,7 +16,7 @@ echo "== RUNNING EXAMPLE 2 --  ATTRIBUTES  =="
 echo "======================================="
 echo ""
 
-./build/example2/Example2 images/portrait.ppm
+./build/example2/Example2 FaceEngineExamples/images/portrait.ppm
 
 echo ""
 echo "======================================="
@@ -24,7 +24,7 @@ echo "== RUNNING EXAMPLE 3 --  MTCNN DETECTOR + ATTRIBUTES  =="
 echo "======================================="
 echo ""
 
-./build/example3/Example3 images/portrait.ppm
+./build/example3/Example3 FaceEngineExamples/images/portrait.ppm
 
 echo ""
 echo ""
