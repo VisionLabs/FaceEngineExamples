8afaccfc931689b3fef7853cbafc5a80b5bdeb01
sava <a.savinskiy@visionlabs.ru>
2017-04-27 18:43:18 +0300

    update RunAllExamples.sh

--- RunAllExamples.sh
+++ RunAllExamples.sh
@@ -27,4 +27,28 @@ echo ""
 ./build/example3/Example3 FaceEngineExamples/images/portrait.ppm
 
 echo ""
+echo "======================================="
+echo "== RUNNING EXAMPLE 6 --  LSH  =="
+echo "======================================="
+echo ""
+
+./build/example6/Example6 FaceEngineExamples/images/ FaceEngineExamples/images/ FaceEngineExamples/images_lists/list.txt 0.7
+
+echo ""
+echo "======================================="
+echo "== RUNNING EXAMPLE 7 --  IO_UTIL  =="
+echo "======================================="
+echo ""
+
+./build/example7/Example7 FaceEngineExamples/images/portrait.ppm
+
+echo ""
+echo "======================================="
+echo "== RUNNING EXAMPLE 8 --  IO_UTIL  =="
+echo "======================================="
+echo ""
+
+./build/example8/Example8 FaceEngineExamples/descriptors/Cameron_Diaz.xpk FaceEngineExamples/descriptors/Cameron_Diaz_2.xpk
+
+echo ""
 echo ""
