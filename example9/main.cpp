1e25029af916f4e6ffdadfa9aec98ee4fc3a4b7f
TimMiftakhov <33287193+TimMiftakhov@users.noreply.github.com>
2017-11-15 17:48:35 +0300

    Update main.cpp

--- example9/main.cpp
+++ example9/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[]) {
 
-	//Capture rgb and depth camera using OPENNI2
+    //Capture rgb and depth camera using OPENNI2
     cv::VideoCapture capture(CV_CAP_OPENNI2);
 
     if (!capture.isOpened()){
@@ -15,10 +15,9 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-	//Turn on pixel remapping
-	if (capture.get(CV_CAP_PROP_OPENNI_REGISTRATION) == 0) capture.set(CV_CAP_PROP_OPENNI_REGISTRATION, 1);
-
-	capture.set(CV_CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, CV_CAP_OPENNI_VGA_30HZ);
+    //Turn on pixel remapping
+    if (capture.get(CV_CAP_PROP_OPENNI_REGISTRATION) == 0) capture.set(CV_CAP_PROP_OPENNI_REGISTRATION, 1);
+    capture.set(CV_CAP_OPENNI_IMAGE_GENERATOR_OUTPUT_MODE, CV_CAP_OPENNI_VGA_30HZ);
 
     using namespace fsdk;
     using namespace lsdk;
@@ -33,7 +32,7 @@ int main(int argc, char* argv[]) {
     IFaceEnginePtr faceEngine = acquire(createFaceEngine("./data", "./data/faceengine.conf"));
     ILivenessEnginePtr livenessEngine = acquire(createLivenessEngine(faceEngine,("./data")));
 
-     //Create liveness according to chosen type
+    //Create liveness according to chosen type
     auto depthLiveness = acquire(livenessEngine->createComplexLiveness(CLA_DEPTH));
 
     std::clog << "Look straight into the camera" << std::endl;
@@ -46,19 +45,19 @@ int main(int argc, char* argv[]) {
     
     while(process){
         //Grab frames
-		capture.grab();
-	    capture.retrieve(capturedImage, CV_CAP_OPENNI_BGR_IMAGE);
-	    capture.retrieve(depthMap, CV_CAP_OPENNI_DEPTH_MAP);
+        capture.grab();
+        capture.retrieve(capturedImage, CV_CAP_OPENNI_BGR_IMAGE);
+        capture.retrieve(depthMap, CV_CAP_OPENNI_DEPTH_MAP);
 
         if((!capturedImage.empty())&&(!depthMap.empty())){
             cv::Mat rgb;
             cv::cvtColor(capturedImage,rgb,CV_BGR2RGB);
             
-			//Wrap images
+            //Wrap images
             Image color(rgb.cols, rgb.rows, fsdk::Format::R8G8B8, rgb.data);
             Image depth(depthMap.cols, depthMap.rows, fsdk::Format::R16, depthMap.data);
             
-			//Update liveness state
+            //Update liveness state
             ResultValue<LSDKError,bool> result = depthLiveness->update(color,depth);
             
             if(result.isOk()){
@@ -73,11 +72,11 @@ int main(int argc, char* argv[]) {
            cv::imshow("Depth", depthMap);
            cv::waitKey(1);
 
-		} else process = false;
+        } else process = false;
     }
 
     std::clog << "Liveness " << (success ? "successfull" : "unsuccessfull") << std::endl;
-	capture.release();
+    capture.release();
     cv::waitKey(0);
     return success;
 }
